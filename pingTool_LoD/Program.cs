945cc9bff728475b77edaa8d067b5c12dfdd8c83
K1netikS <69646118+K1netikS@users.noreply.github.com>
2022-10-07 18:35:44 +0400

    Add files via upload

---
+++ pingTool_LoD/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.NetworkInformation; 
+
+namespace PingProto
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1 - EU, 2 - HKT, 3 - EST");
+
+            string input = Console.ReadLine();
+            int selectedOption;
+            if (int.TryParse(input, out selectedOption))
+            {
+                if (selectedOption == 1)
+                {
+                    try
+                    {
+                        Ping myPing = new Ping();
+                        Console.WriteLine("Eu server:");
+                        PingReply reply = myPing.Send("47.91.87.28", 1000);
+                        if (reply != null)
+                        {
+                            Console.WriteLine("Status :  " + reply.Status + " \nTime : " + reply.RoundtripTime.ToString() + "ms");
+                            //Console.WriteLine(reply.ToString());
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+                    }
+                    Console.ReadKey();
+                }
+
+                else if (selectedOption == 2)
+                {
+                    try
+                    {
+                        Ping myPing = new Ping();
+                        PingReply reply = myPing.Send("47.88.173.11", 1000);
+                        if (reply != null)
+                        {
+                            Console.WriteLine("Status :  " + reply.Status + " \nTime : " + reply.RoundtripTime.ToString() + "ms");
+                            
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+                    }
+                    Console.ReadKey();
+                }
+
+                else if (selectedOption == 3)
+                {
+                    try
+                    {
+                        Ping myPing = new Ping();
+                        PingReply reply = myPing.Send("43.135.190.56", 1000);
+                        if (reply != null)
+                        {
+                            Console.WriteLine("Status :  " + reply.Status + " \nTime : " + reply.RoundtripTime.ToString() + "ms");
+                            
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
